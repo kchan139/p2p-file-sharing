0564610cda758e10b3144d0b4118c3732767b4b2
Khoa Tran <khoatrandd.139@gmail.com>
2025-03-22 22:56:17 +0700

    feat: add class node

--- src/node.py
+++ src/node.py
@@ -1,2 +1,10 @@
 # src/node.py
 
+class Node:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.address = f"{ip}:{port}"
+
+    def update(self, event):
+        print(f"Node {self.address} received update {event['type']}")
