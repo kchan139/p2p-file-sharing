3d1f6397289af6f9a258d5f1ba074d9d49edab76
Khoa Tran <khoatrandd.139@gmail.com>
2025-03-22 23:10:55 +0700

    feat: add torrent class

--- src/torrent.py
+++ src/torrent.py
@@ -1,2 +1,10 @@
 # src/torrent.py
 
+class Torrent:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.pieces = []
+
+    def parse_metainfo(self):
+        # placeholder
+        pass
